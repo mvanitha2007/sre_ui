e4664a776f6d21f52ba6d0c69174ab33bf0c6ad8
Murali <mkmurali.mca@gmail.com>
2023-02-28 23:06:40 +0530

    few more components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MenuitemsComponent } from './menuitems/menuitems.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuitemsComponent,
+    BannerComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menuitems/menuitems.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menuitems',
+  templateUrl: './menuitems.component.html',
+  styleUrls: ['./menuitems.component.css']
+})
+export class MenuitemsComponent {
+
+}
