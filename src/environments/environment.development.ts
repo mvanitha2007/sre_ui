9192e1a5e3fc2922425fa6e871b75e716e2615cd
Murali <mkmurali.mca@gmail.com>
2023-06-03 23:18:34 +0530

    UI Changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    API_URL: "http://localhost:5285/api/",
-    PHOTO_URL: "http://localhost:5285/Photos/"
+    API_URL: "https://srewebapicore.azurewebsites.net/api/",
+    PHOTO_URL: "https://srewebapicore.azurewebsites.net/Photos/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production:false,
+    production:true,
     API_URL: "https://srewebapicore.azurewebsites.net/api/",
     PHOTO_URL: "https://srewebapicore.azurewebsites.net/Photos/"
 };
